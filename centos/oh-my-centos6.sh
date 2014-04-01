4f821c72a5f3570dfdef870ab1d114b75dd66fbc
brain <brain.zhang@netis.com.cn>
2014-04-01 16:26:44 +0800

    add ntpdate.x86_64

--- centos/oh-my-centos6.sh
+++ centos/oh-my-centos6.sh
@@ -121,5 +121,6 @@ sudo yum -y install multitail
 sudo yum -y install nethack
 sudo yum -y install tig
 sudo yum -y install daemon
+sudo yum -y install ntpdate.x86_64
 sudo curl http://beyondgrep.com/ack-2.12-single-file > /usr/local/bin/ack && chmod 0755 /usr/local/bin/ack
 pip install --upgrade cdiff
