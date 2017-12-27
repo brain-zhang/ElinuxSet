f8160e6ed2f98b2e3e034a7cb6802de6e0889124
memorybox <memoryboxes@gmail.com>
2017-12-27 13:08:54 +0800

    private private habit

--- ubuntu/oh-my-vim.sh
+++ ubuntu/oh-my-vim.sh
@@ -8,22 +8,14 @@ sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
 libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial --force-yes
 
-sudo apt-get remove vim vim-runtime gvim
-sudo apt-get remove vim-tiny vim-common vim-gui-common
+sudo apt-get install ctags
+sudo apt-get install build-essential cmake python-dev  #编译YCM自动补全插件依赖
+sudo apt-get install silversearcher-ag
 
-cd ~
-hg clone https://code.google.com/p/vim/
-cd vim
-./configure --with-features=huge \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7-config \
-            --enable-perlinterp \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr \
-            --enable-luainterp
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/
-make VIMRUNTIMEDIR=/usr/share/vim/vim74
-sudo make install
+sudo apt-get install nodejs npm
+sudo npm install -g jslint
+sudo npm install jshint -g
+sudo npm install -g eslint eslint-plugin-standard eslint-plugin-promise eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-html babel-eslint
 
 git clone https://github.com/memoryboxes/k-vim.git ~/k-vim
 sudo apt-get install cmake gcc g++ --force-yes
