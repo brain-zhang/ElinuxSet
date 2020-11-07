4aaa0621a9f4cf6c419bfd4cb6f2ee16b35c4583
brain.zhang <brain.zhangbin@outlook.com>
2020-11-07 10:44:22 +0800

    fix:for ubuntu18-ubuntu20

--- ubuntu/oh-my-vim.sh
+++ ubuntu/oh-my-vim.sh
@@ -4,21 +4,21 @@
 #==================================
 # on my vim
 #==================================
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial --force-yes
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev ruby-dev mercurial --force-yes
 
-sudo apt-get install ctags
-sudo apt-get install build-essential cmake python-dev  #编译YCM自动补全插件依赖
-sudo apt-get install silversearcher-ag
+sudo apt-get install -y ctags
+sudo apt-get install -y build-essential cmake python3-dev  #编译YCM自动补全插件依赖
+sudo apt-get install -y silversearcher-ag
 
-sudo apt-get install nodejs npm
+sudo apt-get install -y nodejs npm
 sudo npm install -g jslint
 sudo npm install jshint -g
 sudo npm install -g eslint eslint-plugin-standard eslint-plugin-promise eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-html babel-eslint
 
-git clone https://github.com/memoryboxes/k-vim.git ~/k-vim
-sudo apt-get install cmake gcc g++ --force-yes
-sudo apt-get install ctags --force-yes
+git clone https://github.com/brain-zhang/k-vim.git ~/k-vim
+sudo apt-get install -y cmake gcc g++
+sudo apt-get install -y ctags
 cd ~/k-vim
 sh -x install.sh
